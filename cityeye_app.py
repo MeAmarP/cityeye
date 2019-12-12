0a3d7a2879ec2a6c43805e6c51c66e66dfb1420a
meamarp <appotdar05@gmail.com>
2019-12-12 19:49:16 +0530

    func to detect vehicles

--- cityeye_app.py
+++ cityeye_app.py
@@ -38,16 +38,48 @@ def applyPreprocessing(frame,bgsegm_inst,kern_len):
     proc_frame = cv2.morphologyEx(proc_frame, cv2.MORPH_DILATE, kernel)    
     return proc_frame
 
-def findObjectProps():
+def findObjectProps(proc_frame):
     """
     """
     # Extract connected objects and its Centroid[x,y], stats[BBox(0:3),Area(4)]
-    _, _, stats, centroids = cv2.connectedComponentsWithStats(fgmask)        
+    _, _, stats, centroids = cv2.connectedComponentsWithStats(proc_frame)        
     centroids = np.reshape(centroids[~np.isnan(centroids)],(-1,2))
     return stats, centroids.astype(np.uint16)
 
-def detectVehicles():
-    return
+def detectVehicles(og_frame,stats,centroids,disp_area=None,disp_center=None):
+    left_lane_cnt = 0
+    right_lane_cnt = 0
+    #Now that we have all the detected components/objects and its stats in a frame we will
+    #iterate through each of the detected object and apply rule to get targeted
+    for idx,obj_xy in enumerate(centroids):
+        
+        #Hypothetical line boundry, count vehicles upon crossing 
+        cv2.line(frame,(120,240),(280,240),(0,255,255),2)
+        cv2.line(frame,(340,200),(450,200),(0,255,128),2)
+        
+        #try to filter out actual targeted objects(CARS) based on Area, 
+        if stats[idx,4] > 400 and stats[idx,4] < 8000 :
+            
+            # Logic to count the vehicles passing through line boundry
+            if frame[int(obj_xy[1]),int(obj_xy[0]),1] == 255 and frame[int(obj_xy[1]),int(obj_xy[0]),2] == 255:
+                left_lane_cnt = left_lane_cnt+1                    
+            if frame[int(obj_xy[1]),int(obj_xy[0]),1] == 255 and frame[int(obj_xy[1]),int(obj_xy[0]),2] == 128:
+                right_lane_cnt = right_lane_cnt+1
+                
+            #======================Put Info on Frame=======================
+            
+            # Centroid Vals of the detected objects in a frame
+#            centr_str = str(obj_xy[0].astype(np.uint32))+","+str(obj_xy[1].astype(np.uint32))
+#                cv2.putText(frame,centr_str,(int(obj_xy[0]),int(obj_xy[1])),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255))
+            
+            # Area Vals of the detected objects in a frame
+#            area_str = str(stats[idx,4])
+#                cv2.putText(frame,area_str,(int(obj_xy[0]),int(obj_xy[1]+20)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,240,0))
+            
+            # Update counter info
+            cv2.putText(frame,'Left Lane Count:'+str(left_lane_cnt),(20,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(128,0,255),2)
+            cv2.putText(frame,'Right Lane Count:'+str(right_lane_cnt),(470,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,128),2)
+    return frame
 
 
 start_time = time.time()
