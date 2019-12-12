4a85753bbf04a3ecf2373035418737911a6f9694
fatninja <appotdar05@gmail.com>
2019-12-13 00:18:51 +0530

    minor changes

--- cityeye_app.py
+++ cityeye_app.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 import time
 
-path_to_video = 'vids/trim_Hway_traffic.mp4'
+path_to_video = 'vids/trim_road_traffic.mp4'
 cap = cv2.VideoCapture(path_to_video)
 
 
@@ -83,7 +83,7 @@ def detectVehicles(og_frame,stats,centroids,disp_area=None,disp_center=None):
 
 
 start_time = time.time()
-fgbg = cv2.bgsegm.createBackgroundSubtractorCNT()
+fgbg = cv2.bgsegm.createBackgroundSubtractorCNT(minPixelStability=15)
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
 left_lane_cnt = 0
 right_lane_cnt = 0
@@ -94,7 +94,7 @@ while cap.isOpened():
     if ret == True:
         #==================Perform preprocessing==============================
         # Apply smooth filter to reduce any noise
-        frame = cv2.GaussianBlur(frame, (5, 5), 0)
+        frame = cv2.GaussianBlur(frame, (3, 3), 0)
         
         #Apply background degmentation algo.
         fgmask = fgbg.apply(frame)
@@ -104,45 +104,45 @@ while cap.isOpened():
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_DILATE, kernel)
         
-        # Extract connected objects and its Centroid[x,y], stats[BBox(0:3),Area(4)]
-        nlabels, _, stats, centroids = cv2.connectedComponentsWithStats(fgmask)        
-        centroids = np.reshape(centroids[~np.isnan(centroids)],(-1,2))
-        
-        #Now that we have all the detected components/objects and its stats in a frame we will
-        #iterate through each of the detected object and apply rule to get targeted
-        for idx,obj_xy in enumerate(centroids):
-            
-            #Hypothetical line boundry, count vehicles upon crossing 
-            cv2.line(frame,(120,240),(280,240),(0,255,255),2)
-            cv2.line(frame,(340,200),(450,200),(0,255,128),2)
-            
-            #try to filter out actual targeted objects(CARS) based on Area, 
-            if stats[idx,4] > 400 and stats[idx,4] < 8000 :
-                
-                # Logic to count the vehicles passing through line boundry
-                if frame[int(obj_xy[1]),int(obj_xy[0]),1] == 255 and frame[int(obj_xy[1]),int(obj_xy[0]),2] == 255:
-                    left_lane_cnt = left_lane_cnt+1                    
-                if frame[int(obj_xy[1]),int(obj_xy[0]),1] == 255 and frame[int(obj_xy[1]),int(obj_xy[0]),2] == 128:
-                    right_lane_cnt = right_lane_cnt+1
-                    
-                #======================Put Info on Frame=======================
-                
-                # Centroid Vals of the detected objects in a frame
-                centr_str = str(obj_xy[0].astype(np.uint32))+","+str(obj_xy[1].astype(np.uint32))
-#                cv2.putText(frame,centr_str,(int(obj_xy[0]),int(obj_xy[1])),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255))
-                
-                # Area Vals of the detected objects in a frame
-                area_str = str(stats[idx,4])
-#                cv2.putText(frame,area_str,(int(obj_xy[0]),int(obj_xy[1]+20)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,240,0))
-                
-                # Update counter info
-                cv2.putText(frame,'Left Lane Count:'+str(left_lane_cnt),(20,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(128,0,255),2)
-                cv2.putText(frame,'Right Lane Count:'+str(right_lane_cnt),(470,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,128),2)
-
-                
-#        OutVideo.write(frame)
-#        cv2.imshow('Video', fgmask)
-        cv2.imshow('ProVideo', frame)
+#        # Extract connected objects and its Centroid[x,y], stats[BBox(0:3),Area(4)]
+#        nlabels, _, stats, centroids = cv2.connectedComponentsWithStats(fgmask)        
+#        centroids = np.reshape(centroids[~np.isnan(centroids)],(-1,2))
+#        
+#        #Now that we have all the detected components/objects and its stats in a frame we will
+#        #iterate through each of the detected object and apply rule to get targeted
+#        for idx,obj_xy in enumerate(centroids):
+#            
+#            #Hypothetical line boundry, count vehicles upon crossing 
+#            cv2.line(frame,(120,240),(280,240),(0,255,255),2)
+#            cv2.line(frame,(340,200),(450,200),(0,255,128),2)
+#            
+#            #try to filter out actual targeted objects(CARS) based on Area, 
+#            if stats[idx,4] > 400 and stats[idx,4] < 8000 :
+#                
+#                # Logic to count the vehicles passing through line boundry
+#                if frame[int(obj_xy[1]),int(obj_xy[0]),1] == 255 and frame[int(obj_xy[1]),int(obj_xy[0]),2] == 255:
+#                    left_lane_cnt = left_lane_cnt+1                    
+#                if frame[int(obj_xy[1]),int(obj_xy[0]),1] == 255 and frame[int(obj_xy[1]),int(obj_xy[0]),2] == 128:
+#                    right_lane_cnt = right_lane_cnt+1
+#                    
+#                #======================Put Info on Frame=======================
+#                
+#                # Centroid Vals of the detected objects in a frame
+#                centr_str = str(obj_xy[0].astype(np.uint32))+","+str(obj_xy[1].astype(np.uint32))
+##                cv2.putText(frame,centr_str,(int(obj_xy[0]),int(obj_xy[1])),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255))
+#                
+#                # Area Vals of the detected objects in a frame
+#                area_str = str(stats[idx,4])
+##                cv2.putText(frame,area_str,(int(obj_xy[0]),int(obj_xy[1]+20)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,240,0))
+#                
+#                # Update counter info
+#                cv2.putText(frame,'Left Lane Count:'+str(left_lane_cnt),(20,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(128,0,255),2)
+#                cv2.putText(frame,'Right Lane Count:'+str(right_lane_cnt),(470,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,128),2)
+#
+#                
+##        OutVideo.write(frame)
+##        cv2.imshow('Video', fgmask)
+        cv2.imshow('ProVideo', fgmask)
         
         k = cv2.waitKey(30) & 0xff
         if k == 27:
